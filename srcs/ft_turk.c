cea87364a26740985c04a041874691fbee627da2
Michel Braganca <miguel.bmiranda21@gmail.com>
2025-06-24 18:44:09 +0100

    improvement

--- srcs/ft_turk.c
+++ srcs/ft_turk.c
@@ -5,13 +5,18 @@ void	ft_turk(t_stack **a, t_stack **b)
 	int	stack_size;
 	
 	stack_size = ft_list_size(a);
-	if (stack_size > 3 && !ft_sort_checker(*a))
+	if (stack_size-- > 3 && !ft_sort_checker(*a))
 		ft_pb(a, b);
-	if (stack_size > 3 && !ft_sort_checker(*a))
+	if (stack_size-- > 3 && !ft_sort_checker(*a))
 		ft_pb(a, b);
-	while (!sort_checker)
+	while (stack_size-- > 3 && !sort_checker)
 	{
 		prep_stacks(a, b);
 
+	}
+	ft_sort_three(a);
+	while (*b)
+	{
+	
 	}
 }
