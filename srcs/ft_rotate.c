a332bbe98a59b18d04254cdbf49afcd5952d4f1c
Miguel Pinheiro Lucas Braganca De Miranda <mipinhei@c2r5s6.42porto.com>
2025-07-01 18:22:02 +0100

    trying....

--- srcs/ft_rotate.c
+++ srcs/ft_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mipinhei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 14:29:58 by mipinhei          #+#    #+#             */
-/*   Updated: 2025/07/01 11:15:41 by mipinhei         ###   ########.fr       */
+/*   Updated: 2025/07/01 18:21:51 by mipinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,17 @@ void	ft_rr(t_stack **a, t_stack **b)
 void	ft_ra(t_stack **a, int i)
 {
 	t_stack	*last;
-	t_stack	*temp;
 
 	if (!*a || !(*a)->next)
 		return ;
 	last = ft_list_last(a);
 	if (!last || !last->prev)
 		return ;
-	temp = *a;
+	last->next = *a;
+	(*a)->next->prev = NULL;
 	*a = (*a)->next;
-	(*a)->prev = NULL;
-	last->next = temp;
-	temp->prev = last;
-	temp->next = NULL;
+	last->next->prev = last;
+	last->next->next = NULL;
 	if (i == 1)
 		write(1, "ra\n", 3);
 }
@@ -50,12 +48,11 @@ void	ft_rb(t_stack **b, int i)
 	last = ft_list_last(b);
 	if (!last || !last->prev)
 		return ;
-	temp = *b;
+	last->next = *b;
+	*b->next->prev = NULL;
 	*b = (*b)->next;
-	(*b)->prev = NULL;
-	last->next = temp;
-	temp->prev = last;
-	temp->next = NULL;
+	last->next->prev = last;
+	last->next->next = NULL;
 	if (i == 1)
 		write(1, "rb\n", 3);
 }
